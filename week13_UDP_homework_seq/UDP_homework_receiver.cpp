adfaae00b82f53244dad3653efc93078dc291765
김현준 <83738219+khj2003@users.noreply.github.com>
2025-12-01 13:29:26 +0900

    Add files via upload

---
+++ week13_UDP_homework_seq/UDP_homework_receiver.cpp
@@ -0,0 +1,55 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+using namespace std;
+
+#define SERVERPORT 9000
+#define BUFSIZE    512
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    //윈속 초기화
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+    
+    //소켓 생성
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+
+    //bind()
+    sockaddr_in localaddr;
+    memset(&localaddr, 0, sizeof(localaddr));
+    localaddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &localaddr.sin_addr);
+    localaddr.sin_port = htons(SERVERPORT);
+
+    bind(sock, (sockaddr*)&localaddr, sizeof(localaddr));
+
+    //메시지 수신 준비 변수
+    char buf[BUFSIZE + 1];
+    sockaddr_in senderaddr;
+    int addrlen = sizeof(senderaddr);
+
+    cout << "Receiver 시작\n";
+
+    while (1) {// 메시지 수신 루프, 패킷이 도착할 때까지 무한 대기
+        int ret = recvfrom(// ret = 받은 바이트 수
+            sock,
+            buf,//실제 메시지가 저장됨
+            BUFSIZE,
+            0,
+            (sockaddr*)&senderaddr,
+            &addrlen
+        );
+
+        if (ret == SOCKET_ERROR)
+            continue;
+
+        buf[ret] = 0;
+
+        cout << "[RECV] " << buf << "\n";
+    }
+
+    closesocket(sock);
+    WSACleanup();
+}
---
+++ week13_UDP_homework_seq/UDP_homework_sender.cpp
@@ -0,0 +1,47 @@
+﻿#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include <chrono>
+#include <thread>
+using namespace std;
+
+#define SERVERPORT 9000
+#define BUFSIZE    512
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    //윈속 초기화
+    WSADATA wsa;
+    WSAStartup(MAKEWORD(2, 2), &wsa);
+
+    //UDP 소켓 생성
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    //목적지 주소 설정(receiver 주소, 내 컴퓨터)
+    sockaddr_in serveraddr;
+    memset(&serveraddr, 0, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    inet_pton(AF_INET, "127.0.0.1", &serveraddr.sin_addr);
+    serveraddr.sin_port = htons(SERVERPORT);
+
+    int seq = 0;//반복 전송을 위한 변수 준비
+    char buf[BUFSIZE];
+
+    cout << "Sender 시작\n";
+
+    while (1) {//반복 송신 루프 시작
+        seq++;//시퀀스 번호 증가 + 메시지 문자열 생성
+        sprintf_s(buf, BUFSIZE, "SEQ: %d", seq);
+
+        sendto(sock, buf, (int)strlen(buf), 0,//UDP 메시지 전송
+            (sockaddr*)&serveraddr, sizeof(serveraddr));
+
+        cout << "[SEND] " << buf << "\n";//송신 내용 출력
+
+        this_thread::sleep_for(chrono::milliseconds(500));//전송 간격(0.5초)
+    }
+
+    closesocket(sock);
+    WSACleanup();
+}
